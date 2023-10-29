4c5b3709b9cad98516947b7edc4d7697ec830a72
RyzenFromFire <ryzenfromfire@gmail.com>
2023-10-29 00:08:04 -0400

    connected to server!!!!!1!

--- app/src/main/java/com/atlasync/app/MainActivity.kt
+++ app/src/main/java/com/atlasync/app/MainActivity.kt
@@ -5,7 +5,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -18,6 +18,8 @@ import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+import okhttp3.*
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var scanner: GmsBarcodeScanner
     private lateinit var pref: SharedPreferences
     private lateinit var ipPref: String
+    private lateinit var baseURL: String
+    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,26 +56,13 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
 
-        binding.scanFab.setOnClickListener { view ->
-            scanner.startScan()
-                .addOnSuccessListener { barcode ->
-                    Snackbar.make(view, barcode.rawValue ?: "Invalid Code", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                }
-                .addOnCanceledListener {
-                    Snackbar.make(view, "Scan Canceled", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                }
-                .addOnFailureListener { e ->
-                    Snackbar.make(view, "Scan error: $e", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                }
-        }
+        binding.scanFab.setOnClickListener { view -> doScan(view) }
 
         pref = PreferenceManager.getDefaultSharedPreferences(this)
         ipPref = pref.getString(getString(R.string.ip_pref_key), getString(R.string.default_ip)) ?: getString(R.string.default_ip)
         PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
-        Toast.makeText(this, ipPref, Toast.LENGTH_SHORT).show()
+        baseURL = "http://$ipPref:5000"
+//        Toast.makeText(this, ipPref, Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -98,5 +89,48 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    private fun doScan(view: View) {
+        // Perform Scan
+        scanner.startScan()
+            .addOnSuccessListener { barcode ->
+                Snackbar.make(view, barcode.rawValue ?: "0", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+                val result = getRoomInfo(barcode.rawValue ?: "0")
+                println(result)
+            }
+            .addOnCanceledListener {
+                Snackbar.make(view, "Scan Canceled", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
+            .addOnFailureListener { e ->
+                Snackbar.make(view, "Scan error: $e", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
+    }
+
+    private fun getRoomInfo(id: String) {
+        val request = Request.Builder()
+            .url("$baseURL/room?id=$id")
+            .build()
+        try {
+            val response = client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val respString = response.body()?.string() ?: "Invalid Room"
+                    updateLocation(respString)
+                }
+            })
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+
+    private fun updateLocation(rawLocation: String) {
+        println(rawLocation)
     }
 }
